0223ceb2941d5ea6a610022d4853dc568c43ad03
KacperWardynski <kacper.wardynski@gmail.com>
2022-11-15 21:28:23 -0700

    re-factored ball module, updated unit tests

--- dong/dong_engine/test/dong_engine/ball_test.exs
+++ dong/dong_engine/test/dong_engine/ball_test.exs
@@ -76,17 +76,59 @@ defmodule DongEngine.BallTest do
     assert moved_ball.position.y == 70
   end
 
-  test "bounce/1 returns struct with updated velocity" do
-    position = %Vector{x: 420, y: 69}
-    velocity = %Vector{x: -20, y: 1}
-    radius = 12
+  describe "bounce/4" do
+    @min_velocity 10
+    @max_velocity 30
 
-    bounced_ball =
-      %Ball{position: position, velocity: velocity, radius: radius}
-      |> Ball.bounce(1, 10, :top)
+    setup do
+      %{
+        ball: %Ball{
+          position: %Vector{x: 0, y: 0},
+          velocity: %Vector{x: 5, y: 5},
+          radius: 10
+        }
+      }
+    end
 
-    refute bounced_ball.velocity.x == velocity.x
-    refute bounced_ball.velocity.y == velocity.y
+    test "with :top collision returns an updated velocity downwards", %{ball: ball} do
+      bounced_ball =
+        ball
+        |> Ball.bounce(@min_velocity, @max_velocity, :top)
+
+      refute bounced_ball.velocity.x == ball.velocity.x
+      refute bounced_ball.velocity.y == ball.velocity.y
+      assert bounced_ball.velocity.y >= 0
+    end
+
+    test "with :bottom collision returns an updated velocity upwards", %{ball: ball} do
+      bounced_ball =
+        ball
+        |> Ball.bounce(@min_velocity, @max_velocity, :bottom)
+
+      refute bounced_ball.velocity.x == ball.velocity.x
+      refute bounced_ball.velocity.y == ball.velocity.y
+      assert bounced_ball.velocity.y <= 0
+    end
+
+    test "with :right collision returns an updated velocity to the left", %{ball: ball} do
+      bounced_ball =
+        ball
+        |> Ball.bounce(@min_velocity, @max_velocity, :right)
+
+      refute bounced_ball.velocity.x == ball.velocity.x
+      refute bounced_ball.velocity.y == ball.velocity.y
+      assert bounced_ball.velocity.x <= 0
+    end
+
+    test "with :left collision returns and updated velocity to the right", %{ball: ball} do
+      bounced_ball =
+        ball
+        |> Ball.bounce(@min_velocity, @max_velocity, :left)
+
+      refute bounced_ball.velocity.x == ball.velocity.x
+      refute bounced_ball.velocity.y == ball.velocity.y
+      assert bounced_ball.velocity.x >= 0
+    end
   end
 
 end
